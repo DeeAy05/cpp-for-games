c5cf3bf351120beec732f1a4a52ef70d217c44dd
DeeAy05 <davidakopan005@gmail.com>
2026-07-22 10:40:02 +0200

    Prime Number

---
+++ Prime Number/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main() {
+	int n;
+	bool isPrime;
+	std::cout << "Enter any natural number: ";
+	std::cin >> n;
+
+	while (std::cin.fail() || n <= 0) {
+		std::cerr << "ERROR: invalid symbols or out of scope." << std::endl;
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Enter any natural number: ";
+		std::cin >> n;
+	}
+
+	uint32_t number = static_cast<uint32_t>(n);
+
+	if (number == 1) isPrime = true;
+
+	for (int i = 1; i < number; ++i) {
+		if (number % i == 0 && i != 1) {
+			isPrime = false;
+			break;
+		}
+		else isPrime = true;
+	}
+	if (isPrime) std::cout << number << " is a prime number";
+	else std::cout << number << " is not a prime number";
+
+	return 0;
+}
