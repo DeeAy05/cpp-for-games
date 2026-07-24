c89c544ec91e3352f819bad0e20cbd0b33bf0138
DeeAy05 <davidakopan005@gmail.com>
2026-07-24 16:24:07 +0200

    Update main.cpp

--- Lambda Functions/main.cpp
+++ Lambda Functions/main.cpp
@@ -5,11 +5,12 @@
 int main() {
 	std::vector<int> v = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-	std::remove_if(v.begin(), v.end(), [](int x) {
+	auto cleanup = std::remove_if(v.begin(), v.end(), [](int x) {
 		return x % 2 != 0;
 		});
 
 
+	v.erase(cleanup, v.end());
 
 	for (auto i : v) {
 		std::cout << i << " ";
