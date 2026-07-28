f8a58a05862d3703a72d9642e05cddab123157fc
DeeAy05 <davidakopan005@gmail.com>
2026-07-28 11:30:27 +0200

    Dynamic Array Template

---
+++ Dynamic Array Template/Array.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+
+template<typename T> 
+class Array {
+private:
+	T* m_data{ nullptr };
+	size_t m_size{ 0 };
+	size_t m_capacity{ 0 };
+public:
+	Array() : m_data(new T[8]), m_size(0), m_capacity(8) {}
+
+	Array(size_t size, const T& value) : m_capacity(size), m_size(size) {
+		m_data = new T[m_capacity];
+		for (size_t i = 0; i < m_size; ++i) m_data[i] = value;
+	}
+
+	~Array()
+	{
+		delete[] m_data;
+	}
+
+	Array(const Array& other) : m_data(new T[other.m_capacity]), m_size(other.m_size), m_capacity(other.m_capacity) 
+	{
+		std::copy(other.m_data, other.m_data + other.m_size, m_data);
+	}
+	Array(Array&& other) noexcept
+	{
+		m_data = other.m_data;
+		m_capacity = other.m_capacity;
+		m_size = other.m_size;
+
+		other.m_data = nullptr;
+		other.m_capacity = 0;
+		other.m_size = 0;
+	}
+	Array& operator=(const Array& other) {
+		if (this == &other) return *this;
+		T* newData = new T[other.m_capacity];
+		std::copy(other.m_data, other.m_data + other.m_size, newData);
+		delete[] m_data;
+		m_data = newData;
+		m_size = other.m_size;
+		m_capacity = other.m_capacity;
+		return *this;
+	}
+
+	Array& operator=(Array&& other) noexcept
+	{
+		if (this == &other) return *this;
+
+		delete[] m_data;
+
+		m_data = other.m_data;
+		m_capacity = other.m_capacity;
+		m_size = other.m_size;
+
+		other.m_data = nullptr;
+		other.m_capacity = 0;
+		other.m_size = 0;
+		return *this;
+	}
+
+	void reserve(size_t newCapacity)
+	{
+		if (newCapacity == m_capacity) return;
+		T* newArray = new T[size];
+		if (m_size < size) std::copy(this, this + m_size, newArray);
+		else std::copy(m_data, m_data + newCapacity, newArray);
+		delete[] m_data;
+		m_data = std::move(newArray);
+		m_capacity = newCapacity;
+
+	}
+
+	template <typename Self>
+	T& operator[](this Self&& self, int i) {
+		assert(i < self.m_capacity);
+		return self.m_data[i];
+	}
+
+	void add(const T& data) {
+		if (m_capacity == m_size) reserve(m_capacity * 2);
+		m_data[size] = data;
+		++m_size;
+	}
+	bool isEmpty() const {
+		return m_size == 0;
+	}
+	size_t size() const {
+		return m_size;
+	}
+	size_t capacity() const {
+		return m_capacity;
+	}
+	template <typename Self>
+	T* data(this Self&& self) {
+		return self.m_data;
+	}
+};
---
+++ Dynamic Array Template/main.cpp
@@ -0,0 +1,39 @@
+#include "Array.hpp"
+#include <utility>
+#include <iostream>
+#include <cassert>
+int main()
+{
+	Array<int> arr;
+	std::cout << "Size: " << arr.size() << " Capacity: " << arr.capacity() << std::endl;
+	Array<int> arr1(3, 10);
+	std::cout << "Size: " << arr.size() << " Capacity: " << arr.capacity() << std::endl;
+	for (int i = 0; i < 3; ++i) {
+		std::cout << *(arr1.data() + i) << std::endl;
+	}
+	Array<int> arr2(arr1);
+	for (int i = 0; i < 3; ++i) {
+		assert(*(arr1.data() + i) == *(arr2.data() + i));
+	}
+	std::cout << "Copy constructor is successful" << std::endl;
+
+	Array<int> arr3(std::move(arr2));
+	for (int i = 0; i < 3; ++i) {
+		std::cout << *(arr3.data() + i) << std::endl;
+	}
+	assert(arr2.isEmpty());
+	std::cout << "Move constructor is successful" << std::endl;
+
+	arr = arr3;
+	for (int i = 0; i < 3; ++i) {
+		assert(*(arr.data() + i) == *(arr3.data() + i));
+	}
+	std::cout << "Copy assignment is successful" << std::endl;
+
+	arr = std::move(arr3);
+	for (int i = 0; i < 3; ++i) {
+		std::cout << *(arr.data() + i) << std::endl;
+	}
+	assert(arr3.isEmpty());
+	std::cout << "Move assignment is successful" << std::endl;
+}
